0b955b3ce7501c1dbd83b37b10b990e4cead0abd
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-11-16 18:40:06 +0000

    [Add] 4-27 devise追加,devise Userモデル and view追加,UserにActiveStorage追加,topページにsign_in,sign_upリンク追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+
+  devise_for :users
+
+
+  get 'homes/about' => 'homes/about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
