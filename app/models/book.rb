674198336edbf1cd4c05bc340d18e9e3eccf1bec
KubotaRikuto <114932185+KubotaRikuto@users.noreply.github.com>
2022-11-28 21:36:00 +0000

    [Add] 4-27 nameモデルの一意制と文字数制限,bookモデルのの文字数制限をバリデーションから設定するように変更

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,6 @@ class Book < ApplicationRecord
 
   # バリデーション設定
   validates :title, presence: :true
-  validates :body, presence: :true
+  validates :body, presence: :true, length:{ maximum: 200 }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, length: { minimum: 2 }
+  validates :name, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
 
   has_many :books, dependent: :destroy
 
